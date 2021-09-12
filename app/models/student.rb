afac41e1ce1dc62f4841b37f8b1eb1e45e206fea
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-11 21:12:49 -0400

    added validation to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,10 @@
 class Student < ApplicationRecord
 has_many :enrollments, dependent: :destroy
 has_many :courses, through: :enrollment
+
+validates :name, presence: true, uniqueness: true
+validates :email, presence: true, uniqueness: true
+validates :gender, presence: true
+validates :image, presence: true, uniqueness: true
+validates :Registration_num, presence: true,  numericality: { only_integer: true, greater_than: 0, less_than: 12}
 end
