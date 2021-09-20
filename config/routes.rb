3d5089bc4d0abbf34cd90b7f3bf3221f7e9e4360
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-19 23:47:50 -0400

    added seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+ resources :enrollments
+  resources :students
+  resources :courses
+  resources :instructors
  
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/user_profile", to: "users#show"
-  resources :enrollments
-  resources :students
-  resources :courses
-  resources :instructors
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
