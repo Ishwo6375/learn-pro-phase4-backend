9ac82cd76b21cfb7c880c1bff36d1e979b275b5a
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-20 00:20:32 -0400

    fixing error

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,5 @@ Rails.application.routes.draw do
   resources :courses
   resources :instructors
  
-  post "/signup", to: "users#create"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  get "/user_profile", to: "users#show"
- 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
