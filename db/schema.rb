c25a0925c6a29b740b432092508d1f721c298e03
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-19 22:06:23 -0400

    added migration users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_021003) do
+ActiveRecord::Schema.define(version: 2021_09_20_020439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2021_09_11_021003) do
     t.string "gender"
     t.string "image"
     t.integer "Registration_num"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
